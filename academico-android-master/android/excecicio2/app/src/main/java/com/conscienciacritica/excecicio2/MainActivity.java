6edbcea83270a3dd1d7f117189de4d2de3448b47
AdrieneMM <ad.marianas@hotmail.com>
2017-05-29 12:24:42 -0300

    segunda aula

--- academico-android-master/android/excecicio2/app/src/main/java/com/conscienciacritica/excecicio2/MainActivity.java
+++ academico-android-master/android/excecicio2/app/src/main/java/com/conscienciacritica/excecicio2/MainActivity.java
@@ -2,6 +2,8 @@ package com.conscienciacritica.excecicio2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void salvar(View vi){
+        TextView txt1=(TextView)findViewById(R.id.inputNome);
+        txt1.setText("Adriene");
+        String valor1=txt1.getText().toString();
+        TextView txt2=(TextView)findViewById(R.id.inputTelefone);
+        txt2.setText(valor1);
+    }
+
 }
